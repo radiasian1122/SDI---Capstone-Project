b8a7af727caf9106825130c40e24e5ec4a519528
Casey Houle <c.j.houle75@gmail.com>
2025-09-08 14:39:38 -0700

    migrations successful latest and rollback

--- backend/api/migrations/20250908193749_create_users_table.js
+++ backend/api/migrations/20250908193749_create_users_table.js
@@ -2,28 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('users', table => {
-      table.integer('dod_id.').unique().notNullable().primary()
-      table.string('username')
-      table.string('password')
-      table.string('uic').notNullable()
-      table.foreign('uic').references('units.uic').notNullable().deferrable('deferred').onDelete('CASCADE')
-      table.string('first_name').notNullable()
-      table.string('last_name').notNullable()
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.integer("dod_id").unique().notNullable().primary();
+    table.string("username");
+    table.string("password");
+    table.string("uic").notNullable();
+    table
+      .foreign("uic")
+      .references("units.uic")
+      .deferrable("deferred")
+      .onDelete("CASCADE");
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.alterTable('users', table => {
-      table.dropForeign('uic')
-          .dropForeign('role_id')
-  })
-          .then( () => {
-              return knex.schema.dropTableIfExists('users')
-          })
+exports.down = function (knex) {
+  return knex.schema
+    .alterTable("users", (table) => {
+      table.dropForeign("uic");
+    })
+    .then(() => {
+      return knex.schema.dropTableIfExists("users");
+    });
 };
--- backend/api/migrations/20250908195011_create_users_roles_table.js
+++ backend/api/migrations/20250908195011_create_users_roles_table.js
@@ -2,14 +2,33 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('users_roles')
+exports.up = function (knex) {
+  return knex.schema.createTable("users_roles", (table) => {
+    table.integer("user_id").notNullable();
+    table
+      .foreign("user_id")
+      .references("users.dod_id")
+      .deferrable("deferred")
+      .onDelete("CASCADE");
+    table.integer("role_id").notNullable();
+    table
+      .foreign("role_id")
+      .references("roles.role_id")
+      .deferrable("deferred")
+      .onDelete("CASCADE");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .alterTable("users_roles", (table) => {
+      table.dropForeign("user_id").dropForeign("role_id");
+    })
+    .then((table) => {
+      return knex.schema.dropTableIfExists("users_roles");
+    });
 };
