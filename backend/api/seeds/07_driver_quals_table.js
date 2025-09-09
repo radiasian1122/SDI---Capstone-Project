40cfce6a8a168d8c3393a6420191a4f8cd176637
Casey Houle <c.j.houle75@gmail.com>
2025-09-09 12:10:09 -0700

    qualifications, seed data

--- backend/api/seeds/07_driver_quals_table.js
+++ backend/api/seeds/07_driver_quals_table.js
@@ -1,13 +1,19 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('driver_quals').del()
-  await knex('driver_quals').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("driver_quals").del();
+  await knex("driver_quals").insert([
+    { id: 1, platform: "JLTV", variant: "some-variant" },
+    { id: 2, platform: "1.1", variant: "some-variant" },
+    { id: 3, platform: "STRYKER", variant: "some-variant" },
+    { id: 4, platform: "MRZR", variant: "some-variant" },
+    { id: 5, platform: "ISV", variant: "some-variant" },
+    { id: 6, platform: "LMTV", variant: "some-variant" },
+    { id: 7, platform: "TLC", variant: "some-variant" },
+    { id: 8, platform: "RFSS", variant: "some-variant" },
+    { id: 9, platform: "QUAD", variant: "some-variant" },
   ]);
 };
