30546b5f6ee74bdd86656924069a0f1bcb81ddc1
Casey Houle <c.j.houle75@gmail.com>
2025-09-09 12:28:15 -0700

    qual seed data

---
+++ backend/api/seeds/08_driver_quals_table.js
@@ -0,0 +1,9 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("driver_quals").del();
+  await knex("driver_quals").insert([]);
+};
