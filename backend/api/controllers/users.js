5ea412901f9a94002a394cefe33222e933acb559
Casey Houle <c.j.houle75@gmail.com>
2025-09-09 09:24:33 -0700

    controllers built out

--- backend/api/controllers/users.js
+++ backend/api/controllers/users.js
@@ -1,7 +1,32 @@
-exports.getAllUsers = (req, res) => {
-  // stuff goes here
-}
+const knex = require("../db.js");
 
-exports.getUserById = (req, res) => {
-  // stuff goes here
-}
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await knex("users").select("*");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await knex("users").where("dod_id", req.params.id).first();
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserQualifications = async (req, res) => {
+  try {
+    const quals = await knex("driver_quals").where("user_id", req.params.id);
+    res.json(quals);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
