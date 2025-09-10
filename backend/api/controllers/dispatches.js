7ad75e60cc1bcd5b3a9bf0881048b55111a43251
Casey Houle <c.j.houle75@gmail.com>
2025-09-10 13:34:21 -0700

    getdispatchbyuic added

--- backend/api/controllers/dispatches.js
+++ backend/api/controllers/dispatches.js
@@ -25,8 +25,34 @@ exports.createNewDispatch = (req, res) => {
   console.log("Write code to create a dispatch here");
 };
 
-exports.getDispatchesByUic = (req, res) => {
-  console.log("Write code to get a dispatch by UIC here");
+exports.getDispatchesByUic = async (req, res) => {
+  try {
+    const dispatches = await knex("dispatches as d")
+      .join("vehicles as v", "d.vehicle_id", "=", "v.vehicle_id")
+      .join("users as u", "d.requestor_id", "=", "u.dod_id")
+      .join("units as un", "v.uic", "=", "un.uic")
+      .where("v.uic", req.params.uic)
+      .select(
+        "d.*",
+        "v.platform",
+        "v.variant",
+        "v.bumper_no",
+        "u.first_name as requestor_first_name",
+        "u.last_name as requestor_last_name",
+        "un.common_name as unit_name"
+      );
+
+    if (dispatches.length > 0) {
+      res.status(200).json(dispatches);
+    } else {
+      res.status(404).json({
+        error: `No dispatches found for UIC: ${req.params.uic}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.error(err);
+  }
 };
 
 exports.getDispatchByRequestorId = async (req, res) => {
