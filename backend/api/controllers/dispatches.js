463c9285f18a080c78281f9e09c4f0a6fa1eda94
Casey Houle <c.j.houle75@gmail.com>
2025-09-10 14:18:37 -0700

    createNewDispatch added

--- backend/api/controllers/dispatches.js
+++ backend/api/controllers/dispatches.js
@@ -23,6 +23,39 @@ exports.getDispatchesByDriver = async (req, res) => {
 
 exports.createNewDispatch = (req, res) => {
   console.log("Write code to create a dispatch here");
+  if (!req.body) {
+    res.status(400).json({
+      message: "You must supply a body with this request.",
+    });
+  } else if (
+    !Object.hasOwn(req.body, "requestor_id") ||
+    !Object.hasOwn(req.body, "driver_id") ||
+    !Object.hasOwn(req.body, "vehicle_id")
+  ) {
+    res.status(400).json({
+      message: "Missing required parameters. See /docs endpoint.",
+    });
+  } else {
+    knex("dispatches")
+      .insert(req.body, [
+        "dispatch_id",
+        "requestor_id",
+        "driver_id",
+        "vehicle_id",
+        "approved",
+      ])
+      .then((newDispatch) => res.status(200).send(newDispatch))
+      .catch((err) => {
+        if (err) {
+          res.status(500).send(err.message);
+          console.error(err.message);
+        } else {
+          res.status(500).json({
+            message: "something went wrong",
+          });
+        }
+      });
+  }
 };
 
 exports.getDispatchesByUic = async (req, res) => {
@@ -60,7 +93,6 @@ exports.getDispatchByRequestorId = async (req, res) => {
     const dispatches = await knex("dispatches")
       .where("requestor_id", req.params.requestorId)
       .select("*");
-
 
     if (dispatches.length > 0) {
       res.status(200).json(dispatches);
