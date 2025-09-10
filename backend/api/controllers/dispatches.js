6aa90657a86a4e5d9fd9102b9b594f002bf04398
Casey Houle <c.j.houle75@gmail.com>
2025-09-10 13:20:21 -0700

    getDispatchByRequestorId added

--- backend/api/controllers/dispatches.js
+++ backend/api/controllers/dispatches.js
@@ -22,9 +22,28 @@ exports.getDispatchesByDriver = async (req, res) => {
 };
 
 exports.createNewDispatch = (req, res) => {
-  console.log('Write code to create a dispatch here')
-}
+  console.log("Write code to create a dispatch here");
+};
 
 exports.getDispatchesByUic = (req, res) => {
-  console.log('Write code to get a dispatch by UIC here')
-}
+  console.log("Write code to get a dispatch by UIC here");
+};
+
+exports.getDispatchByRequestorId = async (req, res) => {
+  try {
+    const dispatches = await knex("dispatches")
+      .where("requestor_id", req.params.requestorId)
+      .select("*");
+
+    if (dispatches.length > 0) {
+      res.status(200).json(dispatches);
+    } else {
+      res.status(404).json({
+        error: `No dispatches found for requestor ID: ${req.params.requestorId}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.error(err);
+  }
+};
