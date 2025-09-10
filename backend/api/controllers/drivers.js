c23c3e34429fa5af4cd973e322f8bcf144749037
alec-somers <alecv1528@gmail.com>
2025-09-10 14:57:21 -0700

    worked on drivers endpoints

--- backend/api/controllers/drivers.js
+++ backend/api/controllers/drivers.js
@@ -1,7 +1,14 @@
 const knex = require('../db.js');
 const { formatDrivers } = require('../utils.js');
 
+
+
+
 exports.getAllDrivers = async (req, res) => {
     await formatDrivers(req, res);
+}
+
+exports.getDriverById = async (req, res) => {
+
 }
 
